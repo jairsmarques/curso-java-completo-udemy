f789ddbae7c8d28894843a5c9f0e36dcc8626f3b
Jair de Sousa <jairsmarques1126@gmail.com>
2024-07-19 13:46:07 -0300

    adicionando a aula sobre estrutura condicional

---
+++ estrutura-condicional/src/EstruturaCondicionalSimples.java
@@ -0,0 +1,18 @@
+
+public class EstruturaCondicionalSimples {
+
+	public static void main(String[] args) {
+
+		int x = 5;
+
+		System.out.println("Bom dia!");
+
+		if (x < 0) {// condição resulta em falsa, não vai imnprimir
+			System.out.println("Boa tarde!");
+		}
+
+		System.out.println("Boa noite!");
+
+	}
+
+}
