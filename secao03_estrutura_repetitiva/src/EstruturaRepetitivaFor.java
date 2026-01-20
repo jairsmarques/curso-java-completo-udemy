4f560523e98332bb1cdff7f9112c2e32b3683b37
Jair Marques <jairsmarques1126@gmail.com>
2026-01-20 15:54:51 -0300

    adicionando os exemplos de códigos da aula sobre estrutura repetitiva

---
+++ secao03_estrutura_repetitiva/src/EstruturaRepetitivaFor.java
@@ -0,0 +1,29 @@
+/*
+ * Fazer um programa que lê um valor inteiro N e depois N números inteiros. Ao final, mostra a soma dos 
+ * N números lidos.
+ * 
+ */
+
+import java.util.Scanner;
+
+public class EstruturaRepetitivaFor {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		int soma = 0;
+		for (int i = 0; i < N; i++) {
+			int x = sc.nextInt();
+			soma += x;
+		}
+
+		System.out.println(soma);
+
+		sc.close();
+
+	}
+
+}
---
+++ secao03_estrutura_repetitiva/src/EstruturaRepetitivaWhile.java
@@ -0,0 +1,29 @@
+/*
+ * Fazer um programa que lê números inteiros até que um zero seja lido. Ao final mostra a soma dos números 
+ * lidos.
+ * 
+ */
+
+import java.util.Scanner;
+
+public class EstruturaRepetitivaWhile {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+
+		int soma = 0;
+		while (x != 0) {
+			soma += x;
+			x = sc.nextInt();
+		}
+
+		System.out.println(soma);
+
+		sc.close();
+
+	}
+
+}
